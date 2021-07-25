5eec9241591fa2933d898e018b1045ab54b1b083
Matt <matthewtmuir@gmail.com>
2021-07-25 15:51:23 -0700

    App Working

--- js/site.js
+++ js/site.js
@@ -1,4 +1,11 @@
-//Call Hello World
-function HelloWorld() {
-    alert("Hello World");
+document.getElementById("btnSubmit").addEventListener("click", reverseStr);
+
+function reverseStr() {
+    let str = document.getElementById("userString").value;
+    let arr = str.split("");
+    let revArr = arr.reverse()
+    let returnStr = revArr.join("");
+
+    document.getElementById("alert").classList.remove("invisible");
+    document.getElementById("msg").innerHTML = returnStr;
 }
